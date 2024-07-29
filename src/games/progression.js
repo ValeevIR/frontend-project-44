c0a85e945060f69a52408d7982388f4858bf8a46
Ilgiz Valeev <skif-vi@yandex.ru>
2024-07-30 00:20:09 +0500

    edit variable  progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,13 +2,12 @@ import game from '../index.js';
 import randomInteger from './rnd.js';
 
 const description = 'What number is missing in the progression?';
-const lengthProgresssion = 10;
+const progressionNumber = 10;
 
 const getProgression = (firstElement, step) => {
   const arr = [];
-  // const lengthProgresssion = 10;
   arr[0] = firstElement;
-  for (let i = 0; i < lengthProgresssion - 1; i += 1) {
+  for (let i = 0; i < progressionNumber - 1; i += 1) {
     arr[i + 1] = arr[i] + step;
   }
   return arr;
@@ -17,20 +16,21 @@ const getProgression = (firstElement, step) => {
 const getQuestionAndAnswer = () => {
   const beginInterval = 1;
   const endInterval = 5;
+  const ratioIncrease = 6;
   const beginIntervalHidden = 0;
-  const endIntervalHidden = lengthProgresssion - 1;
+  const endIntervalHidden = progressionNumber - 1;
 
-  const firstElement = randomInteger(beginInterval, endInterval * 6);
+  const firstElement = randomInteger(beginInterval, endInterval * ratioIncrease);
   const step = randomInteger(beginInterval, endInterval);
 
   const hiddenElement = randomInteger(beginIntervalHidden, endIntervalHidden);
 
   const progression = getProgression(firstElement, step);
-  const currecAnswer = progression[hiddenElement];
+  const answer = progression[hiddenElement];
   progression[hiddenElement] = '..';
   const question = progression.join(' ');
 
-  return [question, currecAnswer.toString()];
+  return [question, answer.toString()];
 };
 
 export default () => {
